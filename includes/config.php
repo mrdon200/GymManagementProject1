9775969f2098440ad82652a59a2e6b44b7b33de6
Prathamesh Salunkhe <159111201+mrdon200@users.noreply.github.com>
2025-11-09 12:33:50 +0530

    Update config.php
    the servername is changed

--- includes/config.php
+++ includes/config.php
@@ -2,13 +2,14 @@
 // index.php - Display package details on the index page
 
 // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "saurabh";
+$servername = "database-1.c7cgswusg79x.ap-south-1.rds.amazonaws.com";
+$username = "admin";
+$password = "pratham123";
 $dbname = "gym";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 
