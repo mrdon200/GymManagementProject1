df9e2e962305799ca07795ae2639d3a78a73a639
Prathamesh Salunkhe <159111201+mrdon200@users.noreply.github.com>
2025-11-09 13:24:02 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,7 +1,7 @@
 <?php
 $host = "database-1.c7cgswusg79x.ap-south-1.rds.amazonaws.com";
 $user = "admin"; // Default XAMPP user
-$pass = "pratham"; // Leave empty for XAMPP
+$pass = "pratham123"; // Leave empty for XAMPP
 $db = "gym";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
@@ -10,4 +10,5 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
 
