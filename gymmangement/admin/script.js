2c3e612a02423a254a5bfdc9d689b332898de2a9
Prathamesh Salunkhe <159111201+mrdon200@users.noreply.github.com>
2025-11-09 11:53:43 +0530

    Delete gymmangement directory

--- gymmangement/admin/script.js
+++
@@ -1,9 +0,0 @@
-
-    function toggleSidebar() {
-        let sidebar = document.querySelector('.sidebar');
-        let content = document.querySelector('.main-content');
-        
-        sidebar.classList.toggle('closed');  // Toggle sidebar visibility
-        content.classList.toggle('shifted'); // Move content when sidebar closes
-    }
-
